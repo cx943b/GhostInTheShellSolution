ac28a3b7630526d8085395ae77defd60b5c44104
cx943b <cx943b@gmail.com>
2023-01-08 23:35:02 +0900

    feat: Set Logger
    ScriptService tested using MainView

--- GhostInTheShell/App.xaml.cs
+++ GhostInTheShell/App.xaml.cs
@@ -6,32 +6,72 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using GhostInTheShell.Modules.InfraStructure;
+using GhostInTheShell.Modules.Script;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 using Prism;
 using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 using Prism.Unity;
+
+using Serilog;
 
 namespace GhostInTheShell
 {
     public partial class App : PrismApplication
     {
-        protected override Window CreateShell()
+        protected override Window CreateShell() => Container.Resolve<MainWindow>();
+
+        protected override void InitializeShell(Window shell)
         {
-            Window w = new Window();
-            w.Background = null;
-            w.AllowsTransparency= true;
-            w.WindowStyle = WindowStyle.None;
-            w.ShowInTaskbar = false;
+            base.InitializeShell(shell);
 
-            w.Width = 200;
-            w.Height = 200;
-
-            return w;
+            shell.Show();
         }
+
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+
+            var regionMgr = Container.Resolve<RegionManager>();
+            regionMgr.RegisterViewWithRegion<Views.MainView>(WellknownRegionNames.MainViewRegion);
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+
+            moduleCatalog.AddModule<ScriptModule>();
+        }
+
+        
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            throw new NotImplementedException();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            containerRegistry.RegisterSingleton<IConfiguration>(() => config);
+            containerRegistry.RegisterSingleton<ILoggerFactory>(prov =>
+            {
+                return LoggerFactory.Create(builder =>
+                {
+                    var logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(config)
+                        .CreateLogger();
+
+                    builder
+                        .ClearProviders()
+                        .AddSerilog(logger)
+                        .AddDebug();
+                });
+            });
+            //containerRegistry.Register(typeof(ILogger<>), typeof(Logger<>));
         }
     }
 }
--- GhostInTheShell/ViewModels/MainViewModel.cs
+++ GhostInTheShell/ViewModels/MainViewModel.cs
@@ -1,14 +1,54 @@
-﻿using Prism.Mvvm;
+﻿using GhostInTheShell.Modules.Script;
+using Microsoft.Extensions.Logging;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GhostInTheShell.ViewModels
 {
     internal class MainViewModel : BindableBase
     {
+        readonly IEventAggregator _eventAggregator;
+        readonly IScriptService _scriptService;
 
+        private string? _Text;
+        public string? Text
+        {
+            get { return _Text; }
+            set { SetProperty(ref _Text, value); }
+        }
+
+        private DelegateCommand _ExecuteCommand;
+
+
+
+
+        public ICommand ExecuteCommand => _ExecuteCommand;
+
+        public MainViewModel(ILogger<MainViewModel> logger, IEventAggregator eventAggregator, IScriptService scriptService)
+        {
+            _eventAggregator = eventAggregator;
+            _scriptService = scriptService;
+
+            _ExecuteCommand = new DelegateCommand(OnExecute);
+
+            eventAggregator.GetEvent<PrintWordScriptCommandEvent>().Subscribe(e => Text += e.PrintWord);
+
+            logger.LogTrace("MainViewModel Initialized");
+        }
+
+        private void OnExecute()
+        {
+            Text = "";
+
+            const string script = "\\t[0]이쁜 여자\\p[은;는] 흔하지만\\w[1000] \\t[1]이쁜 남자\\p[은;는] 귀하다구";
+            _scriptService.Execute(script);
+        }
     }
 }
