c4416268077f9817eab0dbaaa4f65538ccaaa03d
cx943b <cx943b@gmail.com>
2023-01-08 17:25:23 +0900

    Feat: Apply PrismLibrary

--- GhostInTheShell/App.xaml.cs
+++ GhostInTheShell/App.xaml.cs
@@ -6,12 +6,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+
+using Prism;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
 namespace GhostInTheShell
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+
     }
 }
