e3fa5fe2a5138468871311aa85e8864923252a96
cx943b <cx943b@gmail.com>
2023-02-04 14:33:04 +0900

    feat: create shellWindow

---
+++ Modules/GhostInTheShell.Modules.Shell/ShellModule.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GhostInTheShell.Modules.InfraStructure;
+using GhostInTheShell.Modules.Shell.Views;
+
+namespace GhostInTheShell.Modules.Shell
+{
+    public sealed class ShellModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionMgr = containerProvider.Resolve<IRegionManager>();
+            regionMgr.RegisterViewWithRegion<ShellView>(WellknownRegionNames.ShellViewRegion);
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterDialogWindow<ShellWindow>();
+        }
+    }
+}
--- Modules/GhostInTheShell.Modules.Shell/Views/ShellView.xaml.cs
+++ Modules/GhostInTheShell.Modules.Shell/Views/ShellView.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GhostInTheShell.Modules.Shell
+namespace GhostInTheShell.Modules.Shell.Views
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// ShellView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class ShellView : UserControl
     {
-        public UserControl1()
+        public ShellView()
         {
             InitializeComponent();
         }
