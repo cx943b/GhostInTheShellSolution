a1dd522675f5da7a6a87adf0521b189f3652d65f
cx943b <cx943b@gmail.com>
2023-05-01 00:13:36 +0900

    feat: ScriptModel

---
+++ Modules/GhostInTheShell.Modules.Script/ScriptModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostInTheShell.Modules.Script
+{
+    internal class ScriptModel
+    {
+        readonly List<ScriptCommandBase> _lstCommand = new List<ScriptCommandBase>();
+
+        public ScriptModel() { }
+
+        public ScriptModel(IEnumerable<ScriptCommandBase> commands)
+        {
+            AddCommands(commands);
+        }
+
+        public void AddCommand(ScriptCommandBase command)
+        {
+            if(command == null) throw new ArgumentNullException(nameof(command));
+
+            _lstCommand.Add(command);
+        }
+        public void AddCommands(IEnumerable<ScriptCommandBase> commands)
+        {
+            if (commands == null) throw new ArgumentNullException(nameof(commands));
+
+            _lstCommand.AddRange(commands);
+        }
+    }
+}
